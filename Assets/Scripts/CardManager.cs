3a0ec3092dee3d772b59ee5d33061689a236c753
Richard Lu <richard.lu@griffithuni.edu.au>
2018-02-05 16:40:09 +1000

    :notebook:
    100% fixed

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -2,12 +2,13 @@
 using System.IO;
 using System.Collections.Generic;
 using System;
-//using UnityEditor; //can't use this in build
+using UnityEditor;
 
 public class CardManager : MonoBehaviour
 {
     public static CardManager Instance;
     public List<UnitAction> allCards = new List<UnitAction>();
+    UnitAction tempAction = new UnitAction();
 
     private void Awake()
     {
@@ -26,7 +27,7 @@ public class CardManager : MonoBehaviour
         //ReadTextFile("Assets/Scripts/ClassVERYSmart.txt", (int)Class.VERYSmart);
         //ReadTextFile("Assets/Scripts/ClassDude.txt", (int)Class.Dude);
         //ReadTextFile("Assets/Scripts/ClassHealthyBoy.txt", (int)Class.HealthyBoy);
-        ReadTextFile("Assets/Scripts/AllCards.txt");
+        ReadTextFile("Assets/Scripts/AllCardsv2.txt");
     }
 
     void ReadTextFile(string file_path)
@@ -37,60 +38,121 @@ public class CardManager : MonoBehaviour
         {
             string inp_ln = inp_stm.ReadLine();
             //Debug.Log(inp_ln);
-            if (inp_ln[0] != '/') allCards.Add(HandleLine(inp_ln));
+            if (inp_ln.Length != 0 && inp_ln[0] != '/') HandleLine(inp_ln);
             // Do Something with the input. 
         }
         inp_stm.Close();
     }
 
-    UnitAction HandleLine(string line)
+    void HandleLine(string line)
     {
-        UnitAction action = new UnitAction();
-        bool takingName = true, takingPrefab = false;
-        string actionName = "";
-        int prefabIndex = 0;   //not going to work with two digit numbers or higher (or negative), could take in string and convert it
-        //prefabName = "Assets/Prefabs/Projectiles/";
-        for (int i = 0; i < line.Length; i++)
+        string[] parts = line.Split(' ');
+        for (int i = 0; i < parts.Length; i++)
         {
-            if (line[i].Equals('-'))
+            switch (parts[i])
             {
-                takingName = false; takingPrefab = true;
-                i++;
-            }
-            if (line[i].Equals(':')) takingPrefab = false;
-
-            if (takingName)
-            {
-                actionName += line[i];
-            }
-            else if (takingPrefab)
-            {
-                prefabIndex = (int)Char.GetNumericValue(line[i]);
-            }
-            else
-            {
-                action.name = actionName;
-                //if (prefabIndex > 0) prefabIndex -= '0'; //char to int conversion
-                action.projectile = PrefabHelper.Instance.projectiles[prefabIndex];
-                //action.projectile = (GameObject)AssetDatabase.LoadAssetAtPath(prefabName, typeof(GameObject));
-                action.manaCost = (int)Char.GetNumericValue(line[i + 2]);
-                action.healthCost = (int)Char.GetNumericValue(line[i + 4]);
-                action.range = (int)Char.GetNumericValue(line[i + 6]);
-                action.damage = (int)Char.GetNumericValue(line[i + 8]);
-                action.aoe = (int)Char.GetNumericValue(line[i + 10]);
-                action.cooldown = (int)Char.GetNumericValue(line[i + 12]);
-                action.initiative = (int)Char.GetNumericValue(line[i + 14]);
-                action.type = (ActionType)(int)Char.GetNumericValue(line[i + 16]);
-                action.actionClass = (Class)(int)Char.GetNumericValue(line[i + 18]);
-                //Debug.Log(action.name);
-                if (action.cooldown == 9)   //richard this code is mega dodgy get on it already the team is suffering
-                {                           //anyway i hacked together this because it doesnt read in -negative numbers the sign breaks it REAl classic stuff :clap: :smirk:
-                    action.cooldown = 0;
-                    action.damage = -action.damage;
-                }
-                return action;
+                case "[Action]":
+                    tempAction = new UnitAction();
+                    break;
+                case "Name:":
+                    for (int x = 1; x < parts[i].Length - 2 ; x++)
+                    {
+                        try
+                        {
+                            tempAction.name += parts[x] + " ";
+                        }
+                        catch (System.IndexOutOfRangeException)
+                        {
+                            Debug.Log("wtf why tho");
+                        }
+                    }
+                    break;
+                case "Prefab:":
+                    if (parts.Length > 1) tempAction.projectile = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Projectiles/" + parts[++i], typeof(GameObject));
+                    break;
+                case "Mana:":
+                    tempAction.manaCost = Int32.Parse(parts[++i]);
+                    break;
+                case "Health:":
+                    tempAction.healthCost = Int32.Parse(parts[++i]);
+                    break;
+                case "Range:":
+                    tempAction.range = Int32.Parse(parts[++i]);
+                    break;
+                case "Damage:":
+                    tempAction.damage = Int32.Parse(parts[++i]);
+                    break;
+                case "AOE:":
+                    tempAction.aoe = Int32.Parse(parts[++i]);
+                    break;
+                case "Cooldown:":
+                    tempAction.cooldown = Int32.Parse(parts[++i]);
+                    break;
+                case "Initiative:":
+                    tempAction.initiative = Int32.Parse(parts[++i]);
+                    break;
+                case "Type:":
+                    tempAction.type = (ActionType) Int32.Parse(parts[++i]);
+                    break;
+                case "ActionClass:":
+                    tempAction.actionClass = (Class) Int32.Parse(parts[++i]);
+                    break;
+                case "[End]":
+                    if (tempAction != null)
+                    {
+                        allCards.Add(tempAction);
+                    }
+                    break;
             }
         }
-        return null;
     }
+
+    //UnitAction HandleLine(string line)
+    //{
+    //    UnitAction action = new UnitAction();
+    //    bool takingName = true, takingPrefab = false;
+    //    string actionName = "", prefabName = "Assets/Prefabs/Projectiles/";
+
+    //    for (int i = 0; i < line.Length; i++)
+    //    {
+    //        if (line[i].Equals('-'))
+    //        {
+    //            takingName = false; takingPrefab = true;
+    //            i++;
+    //        }
+    //        if (line[i].Equals(':')) takingPrefab = false;
+
+    //        if (takingName)
+    //        {
+    //            actionName += line[i];
+    //        }
+    //        else if (takingPrefab)
+    //        {
+    //            prefabName += line[i];
+    //        }
+    //        else
+    //        {
+    //            action.name = actionName;
+    //            //Debug.Log(prefabName);
+    //            action.projectile = (GameObject)AssetDatabase.LoadAssetAtPath(prefabName, typeof(GameObject));
+    //            action.manaCost = (int)Char.GetNumericValue(line[i + 2]);
+    //            action.healthCost = (int)Char.GetNumericValue(line[i + 4]);
+    //            action.range = (int)Char.GetNumericValue(line[i + 6]);
+    //            action.damage = (int)Char.GetNumericValue(line[i + 8]);
+    //            action.aoe = (int)Char.GetNumericValue(line[i + 10]);
+    //            action.cooldown = (int)Char.GetNumericValue(line[i + 12]);
+    //            action.initiative = (int)Char.GetNumericValue(line[i + 14]);
+    //            action.type = (ActionType)(int)Char.GetNumericValue(line[i + 16]);
+    //            action.actionClass = (Class)(int)Char.GetNumericValue(line[i + 18]);
+    //            //Debug.Log(action.name);
+    //            if (action.cooldown == 9)   //richard this code is mega dodgy get on it already the team is suffering
+    //            {                           //anyway i hacked together this because it doesnt read in -negative numbers the sign breaks it REAl classic stuff :clap: :smirk:
+    //                action.cooldown = 0;
+    //                action.damage = -action.damage;
+    //            }
+    //            return action;
+    //        }
+    //    }
+    //    return null;
+    //}
 }
